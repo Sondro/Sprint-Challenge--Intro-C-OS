ad5812a1b67693ee8b91f8933663bfa3b1217480
Sondro <memoiran@hotmail.com>
2018-02-04 21:05:16 -0800

    commit

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <dirent.h>
 
+#include <stdlib.h>
+
+#include <sys/stat.h>
+#include <linux/limits.h>
+
+void printFiles(char *dirName) {
+  DIR *dir;
+  
+  struct dirent *dent;
+  struct stat statBuff;
+
+  char relPath[512];
+  char fullPath[512];
+
+  const char * relPathFormat = (dirName[strlen(dirName) - 1] == '/') ? "%s%s" : "%s/%s";
+  dir = opendir(dirName);
+
+
+}
+
+
 /**
  * Main
  */
@@ -11,8 +32,14 @@ int main(int argc, char **argv)
   // Open directory
 
   // Repeatly read and print entries
+  puts("Length\n");
+  if (argc < 2) { printFIles(".");}
 
   // Close directory
-
+  int i = 1;
+  for (;i < argc; i++)
+  {
+    printFiles(argv[i]);
+  }
   return 0;
 }
